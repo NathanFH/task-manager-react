2b67bb3c42828432224b9e2d4a6ba2c4d148b69f
Nathan <nathanfhenrique@gmail.com>
2025-03-28 15:52:41 -0300

    Task Manager

---
+++ src/assets/components/Button.jsx
@@ -0,0 +1,10 @@
+function Button(props){
+    return( <button> className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium" 
+    {props.children}
+
+    </button>
+    );
+
+}
+
+export default Button;
